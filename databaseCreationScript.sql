238953158d3b28137dcbf31bbf983168e663a9e7
Kenneth Holm <kholm@umc.edu>
2019-12-02 14:27:23 -0600

    Adding back ticks

--- databaseCreationScript.sql
+++ databaseCreationScript.sql
@@ -16,17 +16,17 @@ USE housing_project;
 
 CREATE TABLE housing
                (
-                `id`               int not null auto_increment primary key,
-                guid               char(32) not null,
-                zip_code           int not null,
-                city               char(32) not null,
-                state              char(2) not null,
-                county             char(32) not null,
-                median_age         int not null,
-                total_rooms        int not null,
-                total_bedrooms     int not null,
-                population         int not null,
-                households         int not null,
-                median_income      int not null,
-                median_house_value int not null
+                `id`                 int not null auto_increment primary key,
+                `guid`               char(32) not null,
+                `zip_code`           int not null,
+                `city`               char(32) not null,
+                `state`              char(2) not null,
+                `county`             char(32) not null,
+                `median_age`         int not null,
+                `total_rooms`        int not null,
+                `total_bedrooms`     int not null,
+                `population`         int not null,
+                `households`         int not null,
+                `median_income`      int not null,
+                `median_house_value` int not null
                );
