dc17dd2f5490d393aa98379f2bede48724ea4d9e
Kenneth Holm <kholm@umc.edu>
2019-12-02 14:25:10 -0600

    Updating the instructions

--- databaseCreationScript.sql
+++ databaseCreationScript.sql
@@ -1,6 +1,12 @@
 --
 -- Use this script to create your database
 --
+-- 1. Connect to your local instance of MySQL
+--    a. mysql -u <user> -p
+-- 2. At the mysql> prompt, use source to read
+--    this file and execute the SQL script
+--    a. mysql> source databaseCreationScript.sql
+--
 
 DROP DATABASE IF EXISTS `housing_project`;
 
@@ -10,17 +16,17 @@ USE housing_project;
 
 CREATE TABLE housing
                (
-                `id`               INT NOT NULL auto_increment PRIMARY KEY,
-                guid               NVARCHAR(32) NOT NULL,
-                zip_code           INT(5) NOT NULL,
-                city               nvarchar(32) NOT NULL,
-                state              nvarchar(2) NOT NULL,
-                county             nvarchar(32) NOT NULL,
-                median_age         INT(3) NOT NULL,
-                total_rooms        INT(5) NOT NULL,
-                total_bedrooms     INT(5) NOT NULL,
-                population         INT(5) NOT NULL,
-                households         INT(5) NOT NULL,
-                median_income      INT(8) NOT NULL,
-                median_house_value INT(8) NOT NULL
+                `id`               int not null auto_increment primary key,
+                guid               char(32) not null,
+                zip_code           int not null,
+                city               char(32) not null,
+                state              char(2) not null,
+                county             char(32) not null,
+                median_age         int not null,
+                total_rooms        int not null,
+                total_bedrooms     int not null,
+                population         int not null,
+                households         int not null,
+                median_income      int not null,
+                median_house_value int not null
                );
